0e736781219fa4d88aa2020725fc60db81576fc4
Josh Winter <winterjoshr@gmail.com>
2017-12-05 20:16:51 -0500

    average chunks of pixels instead of a single pixel

--- Ascii.cs
+++ Ascii.cs
@@ -8,9 +8,12 @@ namespace AsciiDotNet
 {
     internal class Ascii
     {
-        private const int scale = 70;
-        private readonly int xscale = 30;
-        private readonly int yscale = 60;
+        private const int scale = 30;
+        private readonly int xscale;
+        private readonly int yscale;
+        private readonly int width;
+        private readonly int height;
+
         private StringBuilder output;
 
         internal Ascii(string filename)
@@ -19,11 +22,10 @@ namespace AsciiDotNet
 
             using (Image<Rgba32> img = Image.Load(filename))
             {
-                /*
                 xscale = img.Width / scale;
-                yscale = (int)((((float)img.Height / img.Width) * xscale) * 1.5);
-                */
-                Console.WriteLine($"xscale: {xscale}, yscale: {yscale}");
+                yscale = (int)((img.Width / scale) * 1.7);
+                width = img.Width;
+                height = img.Height;
                 Process(img);
             }
         }
@@ -35,17 +37,13 @@ namespace AsciiDotNet
 
         private void Process(Image<Rgba32> img)
         {
-            int width = img.Width;
-            int height = img.Height;
 
             for (int y = 0; y < height; y += yscale)
             {
                 for (int x = 0; x < width; x += xscale)
                 {
-                    Rgba32 pixel = img[x, y];
-
-                    int avg = (pixel.R + pixel.G + pixel.B) / 3;
-                    char symbol = Ascii.Symbol(avg, pixel.A);
+                    int gray = ProcessChunk(img, x, y);
+                    char symbol = gray == -1 ? ' ' : Ascii.Symbol(gray);
 
                     output.Append(symbol);
                 }
@@ -54,11 +52,30 @@ namespace AsciiDotNet
             }
         }
 
-        private static char Symbol(int avg, int alpha)
+        private int ProcessChunk(Image<Rgba32> image, int xx, int yy)
         {
-            if(alpha == 0)
-                return ' ';
+            int gray = 0;
+            int transparent = 0;
+            for (int y = yy; y < yy + yscale; ++y)
+            {
+                for (int x = xx; x < xx + xscale; ++x)
+                {
+                    Rgba32 pixel = image[x, y];
+                    if(pixel.A == 0)
+                        ++transparent;
 
+                    gray += (pixel.R + pixel.G + pixel.B) / 3;
+                }
+            }
+
+            if(transparent > (xscale * yscale) / 2)
+                return -1;
+            else
+                return gray / (xscale * yscale);
+        }
+
+        private static char Symbol(int avg)
+        {
             if(avg < 25)
                 return '.';
             if(avg < 50)
